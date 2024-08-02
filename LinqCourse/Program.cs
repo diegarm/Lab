422674f3f8930434679ce303b656be3a0d58bd68
Diego Armando <diego.dearruda@mecalux.com>
2024-08-02 13:32:23 +0200

    LInq Course

---
+++ LinqCourse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ArrayList myArrayList = new ArrayList();
+        myArrayList.Add("hello"); //adding elements
+        myArrayList.Add("298");
+        myArrayList.Reverse(); // reversing the arrayList elements
+
+        Console.WriteLine("Array List Reverse");
+        foreach (var i in myArrayList)
+            Console.WriteLine(i);
+
+
+        Console.WriteLine("Hash Table");
+        Hashtable myHashtable = new Hashtable();
+        myHashtable[0] = "zero"; // adding elements
+        myHashtable[1] = "one";
+
+        foreach (var i in myHashtable)
+            Console.WriteLine(i);
+
+
+        Console.WriteLine("Queue");
+        Queue myQueue = new Queue();
+        myQueue.Enqueue("animal");
+        myQueue.Enqueue("people");
+
+        foreach(var i in myQueue)
+            Console.WriteLine(i);
+
+
+        Console.WriteLine("Stack");
+        var myStack = new Stack();
+        myStack.Push(false); // adding elements
+        myStack.Push("dog");
+        myStack.Peek(); // obtain the value of the top stack element
+
+        foreach(var i in myStack)
+            Console.WriteLine(i);
+    }
+}
